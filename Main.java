56151b395fe6bc3b084634bf6faa5cf4f992d55c
SalahBantal <>
2021-05-20 13:13:21 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+
+public class Main {
+
+  public static void main(String[] args) {
+    int data[]={39,49,29,19,59,69};
+    int min = data[0];
+    int index = 0;
+
+    for(int j=0;j<data.length-1;j++){
+      for(int i = j;i<data.length;i++){
+        if(data[i] < min){
+          min=data[i];
+          index = i;
+
+        }
+        System.out.println(Arrays.toString(data));
+      }
+
+      if(min<data[j]){
+        data[index] = data[j];
+        data[j]=min;
+      }
+      min=data[j+1];
+
+    }
+    
+  }
+}
